65662eac30c6fb93f48d3a6eddbded0d812d28af
Reda Cherfaoui <redacherfaoui@gmail.com>
2024-06-26 15:05:06 +0100

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+import DialogContent from "@/components/DialogContent.vue";
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <main>
+    <h2 class="text-4xl text-gray-600">mon titre</h2>
+    <DialogContent />
+  </main>
+</template>
